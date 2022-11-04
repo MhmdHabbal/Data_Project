6640effb5f2011ac9a963ddcdbef8d601c0c3ea0
MhmdHabbal <mhmdhabbal2001@gmail.com>
2022-11-04 21:57:18 +0200

    It's just a test

---
+++ Node.java
@@ -0,0 +1,16 @@
+package Data_Project;
+
+public class Node {
+    
+    int data;
+    Node next;
+
+    Node() {
+        data = 0;
+        next = null;
+    }
+
+    Node(int data) {
+        this.data = data;
+    }
+}
